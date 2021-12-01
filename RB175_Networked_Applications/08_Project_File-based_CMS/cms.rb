1ab5a5cd4b1aa0f1e3a44661d50f658f551c5ed4
Justin Simpson <JSimpson523@gmail.com>
2021-12-01 17:52:00 +0000

    Implement file create and delete.

--- RB175_Networked_Applications/08_Project_File-based_CMS/cms.rb
+++ RB175_Networked_Applications/08_Project_File-based_CMS/cms.rb
@@ -4,8 +4,6 @@ require 'sinatra/content_for'
 require 'sinatra/reloader'
 require 'tilt/erubis'
 
-DATA_PATH = "#{File.dirname(__FILE__)}/data/".freeze
-
 configure do
   enable :sessions
   set :session_secret, 'super secret'
@@ -20,21 +18,33 @@ def file_contents(file_path)
   file = File.read(file_path)
   case file_path.split('.').last
   when 'txt'
-    headers["Content-Type"] = 'text/plain'
+    headers['Content-Type'] = 'text/plain'
     file
   when 'md'
-    render_markdown(file)
+    erb render_markdown(file)
+  end
+end
+
+def data_path
+  if ENV['RACK_ENV'] == 'test'
+    File.expand_path('../tests/data', __FILE__)
+  else
+    File.expand_path('../data', __FILE__)
   end
 end
 
 get '/' do
-  @dir = Dir.glob("#{DATA_PATH}*")
+  @dir = Dir.glob(File.join(data_path, '*'))
             .map { |file| File.basename(file) }
-  erb :layout
+  erb :index
+end
+
+get '/new' do
+  erb :new
 end
 
 get '/:file_name' do
-  file_path = DATA_PATH + params[:file_name]
+  file_path = File.join(data_path, params[:file_name])
 
   if !File.file?(file_path)
     session[:message] = "#{params[:file_name]} does not exist."
@@ -45,7 +55,7 @@ get '/:file_name' do
 end
 
 get '/:file_name/edit' do
-  file_path = DATA_PATH + params[:file_name]
+  file_path = File.join(data_path, params[:file_name])
 
   @file_name = params[:file_name]
   @content = File.read(file_path)
@@ -53,8 +63,30 @@ get '/:file_name/edit' do
   erb :edit
 end
 
-post '/:file_name/edit' do
-  file_path = DATA_PATH + params[:file_name]
+post '/create' do
+  @new_file_name = params[:new_file_name].to_s
+
+  if @new_file_name.empty?
+    session[:message] = 'A name is required.'
+    status 422
+    erb :new
+  else
+    File.write(File.join(data_path, @new_file_name), '')
+    session[:message] = "#{@new_file_name} was created."
+    redirect '/'
+  end
+end
+
+post '/:file_name/delete' do
+  file_name = File.join(data_path, params[:file_name])
+
+  File.delete(file_name)
+  session[:message] = "#{params[:file_name]} has been deleted."
+  redirect '/'
+end
+
+post '/:file_name' do
+  file_path = File.join(data_path, params[:file_name])
 
   File.write(file_path, params[:content])
 
--- RB175_Networked_Applications/08_Project_File-based_CMS/tests/cms_test.rb
+++ RB175_Networked_Applications/08_Project_File-based_CMS/tests/cms_test.rb
@@ -1,7 +1,8 @@
 ENV['RACK_ENV'] = 'test'
 
-require 'rack/test'
+require 'fileutils'
 require 'minitest/autorun'
+require 'rack/test'
 
 require_relative '../cms'
 
@@ -12,16 +13,34 @@ class CMSTest < Minitest::Test
     Sinatra::Application
   end
 
-  def test_root
+  def setup
+    FileUtils.mkdir_p(data_path)
+  end
+
+  def teardown
+    FileUtils.rm_rf(data_path)
+  end
+
+  def create_document(name, contents = '')
+    File.open(File.join(data_path, name), 'w') do |file|
+      file.write(contents)
+    end
+  end
+
+  def test_index
+    create_document 'about.md'
+    create_document 'changes.txt'
+
     get '/'
     assert_equal 200, last_response.status
     assert_equal 'text/html;charset=utf-8', last_response['Content-Type']
     assert_includes last_response.body, 'about.md'
     assert_includes last_response.body, 'changes.txt'
-    assert_includes last_response.body, 'history.txt'
   end
 
   def test_file_page
+    create_document 'history.txt', '1995 - Ruby 0.95 released.'
+
     get '/history.txt'
     assert_equal 200, last_response.status
     assert_equal 'text/plain', last_response['Content-Type']
@@ -31,16 +50,18 @@ class CMSTest < Minitest::Test
   def test_page_dne_redirect
     get '/dne'
     assert_equal 302, last_response.status
-
+    
     get last_response['Location']
     assert_equal 200, last_response.status
-    assert_includes  last_response.body, "dne does not exist."
-
+    assert_includes last_response.body, "dne does not exist."
+    
     get '/'
     refute_includes last_response.body, 'dne does not exist.'
   end
 
   def test_markdown_document_view
+    create_document 'about.md', '# Ruby is...'
+
     get '/about.md'
     assert_equal 200, last_response.status
     assert_equal 'text/html;charset=utf-8', last_response['Content-Type']
@@ -48,6 +69,8 @@ class CMSTest < Minitest::Test
   end
 
   def test_edit_file
+    create_document 'changes.txt'
+
     get '/changes.txt/edit'
     assert_equal 200, last_response.status
     assert_includes last_response.body, '<textarea'
@@ -55,7 +78,7 @@ class CMSTest < Minitest::Test
   end
 
   def test_update_file
-    post '/changes.txt', content: 'new_content'
+    post '/changes.txt', content: 'new content'
     assert_equal 302, last_response.status
 
     get last_response['Location']
@@ -63,6 +86,44 @@ class CMSTest < Minitest::Test
 
     get '/changes.txt'
     assert_equal 200, last_response.status
-    assert_includes last_response.body 'new content'
+    assert_includes last_response.body, 'new content'
+  end
+
+  def test_new_document_form
+    get '/new'
+
+    assert_equal 200, last_response.status
+    assert_includes last_response.body, '<input'
+    assert_includes last_response.body, %q(<button type='submit')
+  end
+
+  def test_create_file
+    post '/create', new_file_name: 'test.txt'
+    assert_equal 302, last_response.status
+
+    get last_response['Location']
+    assert_includes last_response.body, 'test.txt was created.'
+
+    get '/'
+    assert_includes last_response.body, 'test.txt'
+  end
+
+  def test_nameless_new_file
+    post '/create', new_file_name = ''
+    assert_equal 422, last_response.status
+    assert_includes last_response.body, 'A name is required.'
+  end
+
+  def test_deleting_file
+    create_document 'delete_me.txt'
+
+    post '/delete_me.txt/delete'
+    assert_equal 302, last_response.status
+
+    get last_response['Location']
+    assert_includes last_response.body, 'delete_me.txt has been deleted.'
+
+    get '/'
+    refute_includes last_response.body, 'delete_me.txt'
   end
 end
